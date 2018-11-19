14154fe9e6daf8ed3d530327e3f38812f099dd4c
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-19 18:20:31 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -63,14 +63,16 @@ end
 def runner
   # code runner here
   welcome;
-  initial_round;
-  card_total = hit? (initial_round);
-  until card_total == 21
-    hit? (card_total);
+  card_total = initial_round;
+  
+  until card_total >= 21
+    card_total = hit? (card_total);
     display_card_total (card_total)
     
   end
   end_game(card_total)
   
 end
+
+
     
