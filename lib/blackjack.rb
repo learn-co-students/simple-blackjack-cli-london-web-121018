39208ff903a72cdbcd2e0f96c4101c96547fb89e
github username <github email address>
2018-11-26 15:44:56 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,54 +1,63 @@
+
+require 'pry'
 def welcome
   # code #welcome here
   puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  randomNumber = rand(1..11)
+  # code #deal_card here
+  deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11]
+  deck.sample
 end
 
-def display_card_total(total_cards)
-
-  puts "Your cards add up to #{total_cards}"
-  return total_cards
+def display_card_total(total)
+  # code #display_card_total here
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
+  # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  letter = gets.chomp
+  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game(card_total)
-  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+def end_game(total)
+  # code #end_game here
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
-  initOne = deal_card()
-  initTwo = deal_card()
-  sumInit = initOne + initTwo
-  display_card_total(sumInit)
-
+  # code #initial_round here
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
+  card_total
 end
 
-def hit?(myNumber)
-  prompt_user()
-  result = get_user_input()
-  card_total = myNumber
-    if result == 's'
-      return myNumber
-    elsif result == 'h'
-      sumInit = myNumber + deal_card()
-      return sumInit
-    else
-      invalid_command()
-    end
+def hit?(total)
+  # code hit? here
+  prompt_user
+  action = get_user_input()
+
+  if action == "s"
+    total
+  elsif action == "h"
+    total += deal_card
+  else
+    invalid_command()
+  end
+
+  total
 end
 
 def invalid_command
-  puts "Please enter a valid command"
+  # code invalid_command here
+  puts "I don't know what you mean. Try again."
+  prompt_user
 end
 
 #####################################################
@@ -56,12 +65,14 @@ end
 #####################################################
 
 def runner
- welcome()
- number = initial_round()
-  until number > 21
-   hit?(number)
-   display_card_total(number)
-  number += hit?(number)
+  # code runner here
+  welcome
+  card_total = initial_round()
+
+  until card_total >= 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
   end
-  end_game(number)
+  end_game(card_total)
+
 end
